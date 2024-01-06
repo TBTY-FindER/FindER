abca4395a263264a0335b5db3561bc52a6c297ee
Krish <krishlal4204@gmail.com>
2024-01-06 11:51:03 -0700

    modelled objects as classes

---
+++ Backend/AHS/Hospital.js
@@ -0,0 +1,14 @@
+
+
+class Hospital {
+    constructor(Name,Category,WaitTime,URL,Note,TimesUnavailable){
+        this.Name = Name;
+        this.Category = Category;
+        this.WaitTime = WaitTime;
+        this.URL = URL;
+        this.Note = Note;
+        this.TimesUnavailable = TimesUnavailable;
+    }
+}
+
+module.exports = Hospital
