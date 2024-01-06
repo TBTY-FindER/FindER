78d14738da838f897d3a5313acbf885ed51c94bd
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2024-01-06 11:53:25 -0700

    permission check done

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,10 +3,10 @@ import React from "react";
 const Loader = () => {
   return (
     <div id="container">
-      <label class="loading-title">Checking for permission...</label>
-      <span class="loading-circle sp1">
-        <span class="loading-circle sp2">
-          <span class="loading-circle sp3"></span>
+      <label className="loading-title">Checking for permission...</label>
+      <span className="loading-circle sp1">
+        <span className="loading-circle sp2">
+          <span className="loading-circle sp3"></span>
         </span>
       </span>
     </div>
