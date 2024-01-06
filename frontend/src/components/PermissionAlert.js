839e44a885d5e2b9cc6749cf1d88dd8368f887bc
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2024-01-06 12:02:36 -0700

    permission fixed

--- frontend/src/components/PermissionAlert.js
+++ frontend/src/components/PermissionAlert.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./components.css";
 
 const PermissionAlert = () => {
+  const requestMicrophonePermission = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      // Microphone permission granted and audio stream obtained
+    } catch (error) {
+      // Handle the error (e.g., permission denied)
+    }
+  };
+  const requestLocationPermission = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Position obtained
+        },
+        (error) => {
+          // Handle the error (e.g., permission denied)
+        }
+      );
+    } else {
+      // Geolocation is not supported by this browser
+    }
+  };
+
+  // ask for permission to use microphone and location
+
+  const handleMicrophoneAccess = async () => {
+    await requestMicrophonePermission();
+  };
+
+  const handleLocationAccess = () => {
+    requestLocationPermission();
+  };
+
+  useEffect(() => {
+    handleLocationAccess();
+    handleMicrophoneAccess();
+  }, []);
+
   return (
     <div className="card-permission">
       <button type="button" className="dismiss">
