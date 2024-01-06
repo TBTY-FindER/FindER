9caa652ea188a4e21237ea7093afc262e5682f03
Meharpreet Singh Nanda <msnanda515@gmail.com>
2024-01-06 12:50:08 -0700

    Fix cors error

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const HospitalController = require('./Controller/hospitalController');
 const router = express.Router();
-router.use(cors());
 
 // Define your routes
 router.get('/hospitals', (req, res) => {
