7aae4dc5a4304479fe0a81e07f0f8b23565bb607
Krish <krishlal4204@gmail.com>
2024-01-06 17:18:27 -0700

    add parallel promise execution to work around matrix api reqs

--- Backend/Maps/DistanceFinder.js
+++ Backend/Maps/DistanceFinder.js
@@ -1,41 +1,71 @@
+require("dotenv").config();
 const AHS = require("../AHS/AHS")
 const {Client, LatLng} = require("@googlemaps/google-maps-services-js");
 const GOOGLE_MAPS_API = process.env.GOOGLE_MAPS_API_KEY
 const client = new Client({});
 const ahs = new AHS()
-
+const HospitalLatLngs = require("./hospital_lat_lng.json")
 testorigin = {
     lat: 53.5281882,
     long: -113.5301719
 }
 
-function getHospitalLatLng(destinations){
-    
-} 
-
 async function DistanceToEverything(origin){
     let hospitals;
-    hospitals = await ahs.buildClasses()
+    // hospitals = await ahs.buildClasses()
     // console.log(hospitals)
-    let arrs = []
-    for (const i of hospitals){
-        arrs.push(i.Name)
+    let dest = []
+    for (const i in HospitalLatLngs){
+        dest.push({lat:HospitalLatLngs[i].lat,lng:HospitalLatLngs[i].lng})
     }
-    dmatrixRequest = {
-        params: {
-            origins: [{lat: origin.lat,long: origin.long}],
-            destinations: arrs,
-            key: GOOGLE_MAPS_API
-        },
-        timeout:1000
+    const half = Math.ceil(dest.length/2)
+    const requests = [
+        client.distancematrix({
+            params: {
+                origins: [{ lat: origin.lat, lng: origin.long }],
+                destinations: dest.slice(0, half),
+                key: GOOGLE_MAPS_API
+            },
+            timeout: 1000
+        }),
+
+        client.distancematrix({
+            params: {
+                origins: [{ lat: origin.lat, lng: origin.long }],
+                destinations: dest.slice(half),
+                key: GOOGLE_MAPS_API
+            },
+            timeout: 1000
+        })
+    ];
+    try{
+        const [res1,res2] = await Promise.all(requests)
+        console.log(res1.data)
+        console.log(res2.data)
     }
-    client.distancematrix(
-        dmatrixRequest
-    ).then((result) => {
-      console.log(result.data)
-    }).catch((err) => {
-        console.log(err)
-    });
+    catch{
+        (err) => {
+            console.log(err)
+        }
+    }
+    // dmatrixRequest = {
+    //     params: {
+    //         origins: [{lat: origin.lat,lng: origin.long}],
+    //         destinations: dest.slice(0,dest.length/2),
+    //         key: GOOGLE_MAPS_API
+    //     },
+    //     timeout:1000
+    // }
+    // client.distancematrix(
+    //     dmatrixRequest
+    // ).then((result) => {
+    //   console.log(result.data)
+    //   for (const i of result.data.rows){
+    //     console.log(i.elements)
+    //   }
+    // }).catch((err) => {
+    //     console.log(err)
+    // });
 
 }
 DistanceToEverything(testorigin)
